4841cbed36a195aaea957b9e8f8f43d7d8e3e6e8
Justin Jagielski <jjagiels@udel.edu>
2018-10-10 16:48:46 -0400

    began working on client, added a basic UI, and started working through the user options

--- tcpclient.c
+++ tcpclient.c
@@ -1,8 +1,10 @@
 /* tcp_ client.c */ 
 /* Programmed by Adarsh Sethi */
+/* Modified by Justin Jagielski and Marco Arcilla */
 /* Sept. 13, 2018 */     
 
 #include <stdio.h>          /* for standard I/O functions */
+#include <ctype.h>          /* for toupper() function */
 #include <stdlib.h>         /* for exit */
 #include <string.h>         /* for memset, memcpy, and strlen */
 #include <netdb.h>          /* for struct hostent and gethostbyname */
@@ -20,10 +22,16 @@ int main(void) {
                                         stores server address */
    struct hostent * server_hp;      /* Structure to store server's IP
                                         address */
+                                        
+   enum state{main, checkBalance, deposit, withdraw, transfer}menu;
+   enum input{C,D,W,T}userSelection;
+   menu = main;
    char server_hostname[STRING_SIZE]; /* Server's hostname */
    unsigned short server_port;  /* Port number used by server (remote port) */
 
-   char sentence[STRING_SIZE];  /* send message */
+   char selection[1];  /* sent command to server */
+   
+   char sentence[STRING_SIZE];
    char modifiedSentence[STRING_SIZE]; /* receive message */
    unsigned int msg_len;  /* length of message */                      
    int bytes_sent, bytes_recd; /* number of bytes sent or received */
@@ -71,12 +79,62 @@ int main(void) {
       exit(1);
    }
   
-   /* user interface */
+   /* original user interface */
 
-   printf("Please input a sentence:\n");
+   /*printf("Please input a sentence:\n");
    scanf("%s", sentence);
-   msg_len = strlen(sentence) + 1;
-
+   msg_len = strlen(sentence) + 1; */
+   
+   /* our banking user interface */
+   while(1){
+      switch(menu){
+          case main:
+            printf("Please select an action:\nCheck the balance of an account: C\nDeposit an amount into an account: D\nWithdraw an amount from an account: W\nTransfer an amount from one account to another: T\n:");
+            scanf("%s", selection);
+        
+            toupper(selection[0]); //TODO: This is not forcing the input to an uppercase letter.
+            
+            userSelection = selection[0];
+        
+            switch(userSelection){
+        
+                case 'C':
+                    menu = checkBalance;
+                    break;
+                case 'D':
+                    menu = deposit;
+                    break;
+                case 'W':
+                    menu = withdraw;
+                    break;
+                case 'T':
+                    menu = transfer;
+                    break;
+                default:
+                    printf("Invalid selection, please try again");
+                    menu = main;
+                    break;
+            }
+            break;
+          case checkBalance:
+              //TODO: Code Here
+              break;
+          case deposit:
+              //TODO: Code Here
+              break;
+          case withdraw:
+              //TODO: Code Here
+              break;
+          case transfer:
+              //TODO: Code Here
+              break;
+          default:
+              printf("menu enum not working correctly! Fix it!");
+              exit(0);
+              break;
+      }
+   }
+   
    /* send message */
    
    bytes_sent = send(sock_client, sentence, msg_len, 0);
